0c450f1626e7510bc83fc7204aa2a643e9b99643
Sam <bobbobson79@outlook.com>
2024-03-20 17:56:01 +0000

    update

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    
+    doc = fetch('https://api.thecatapi.com/v1/images/search')
+    console.log(doc)
+    //document.getElementById('cat').src = 
 })
