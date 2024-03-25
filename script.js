255af76473ea84c705c4807fec8b28ab9f7d4d7a
Sam <bobbobson79@outlook.com>
2024-03-25 17:12:51 +0000

    added searching for dogs and made the ui better

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-async function getCat(data){
-    data = await fetch('https://api.thecatapi.com/v1/images/search');
-    cat = await data.json()
-    document.getElementById('cat').setAttribute('src', cat[0].url)
-    document.getElementById('cat').style.width = '50%'
-    document.getElementById('cat').style.height = '50%'
+async function getAnimal(){
+    updateURL(currentAnimal)
+    data = await fetch(url);
+    response = await data.json()
+    document.getElementById('image').setAttribute('src', response[0].url)
+    document.getElementById('image').style.width = '50%'
+    document.getElementById('image').style.height = '50%'
 }
 
 async function refreshPicture(){
-    document.getElementById('cat').setAttribute('src', "loading.gif")
-    data = await fetch('https://api.thecatapi.com/v1/images/search');
-    cat = await data.json()
-    document.getElementById('cat').setAttribute('src', cat[0].url)
-    document.getElementById('cat').style.width = '50%'
-    document.getElementById('cat').style.height = '50%'
+    await document.getElementById('image').setAttribute('src', "assets/loading.gif")
+    data = await fetch(url);
+    response = await data.json()
+    document.getElementById('image').setAttribute('src', response[0].url)
+    document.getElementById('image').style.width = '50%'
+    document.getElementById('image').style.height = '50%'
+}
+
+async function catPress(){
+    currentAnimal = 'cat'
+    catElement = document.getElementById('catBut')
+    dogElement = document.getElementById('dogBut')
+    animalElement = document.getElementById('animal')
+
+    catElement.style.backgroundColor = 'green'
+    dogElement.style.backgroundColor = 'red'
+
+    await updateURL(currentAnimal)
+
+    animalElement.innerHTML = String(currentAnimal)
+}
+
+async function dogPress(){
+    currentAnimal = 'dog'
+    catElement = document.getElementById('catBut')
+    dogElement = document.getElementById('dogBut')
+    animalElement = document.getElementById('animal')
+
+    catElement.style.backgroundColor = 'red'
+    dogElement.style.backgroundColor = 'green'
+
+    await updateURL(currentAnimal)
+
+    animalElement.innerHTML = String(currentAnimal)
+}
+
+async function updateURL(currentAnimal){
+    url = 'https://api.the' + currentAnimal + 'api.com/v1/images/search'
+    return url;
 }
 
 let data = ''
+let currentAnimal = 'cat'
 
-getCat(data)
+
+
+
+getAnimal()
